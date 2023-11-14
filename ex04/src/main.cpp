ef7eaa2dd1fbdd07c9d3c71d73a4abd006e4ddc1
Dmitrii Larionov <mikrolux@gmail.com>
2023-11-14 17:06:36 +0700

    04 шт цщкл

---
+++ ex04/src/main.cpp
@@ -0,0 +1,32 @@
+
+
+#include <iostream>
+#include <fstream>
+
+int main() {
+	std::ifstream myfile1;
+	std::ofstream myfile2;
+	std::string temp;
+	std::string s1;
+	std::string s2;	
+
+	s1 = "hello";
+	s2 = "bye";
+	myfile1.open("01.txt");
+	myfile2.open("02.txt");
+	
+	if (myfile1.is_open() && myfile2.is_open())
+	{
+		myfile1 >> temp;
+		while(temp.find(s1))
+		{
+			
+		}
+		myfile2 << temp;
+	}
+	// myfile1 << 
+	myfile1.close();
+	myfile2.close();
+	return(0);
+
+}
