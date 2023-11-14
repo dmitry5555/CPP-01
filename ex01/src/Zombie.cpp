9537a7844cbde30be6ae11233d16e5d3dec65f91
Dmitrii Larionov <mikrolux@gmail.com>
2023-11-14 19:16:18 +0700

    05 done 80%

--- ex01/src/Zombie.cpp
+++ ex01/src/Zombie.cpp
@@ -1,7 +1,5 @@
 #include "Zombie.hpp"
 
-
-
 Zombie::Zombie() {
 }
 
---
+++ ex05/src/main.cpp
@@ -0,0 +1,9 @@
+#include "Harl.hpp"
+
+int main() {
+	Harl* harl = new Harl;
+
+	harl->complain("debug");
+	harl->complain("info");
+
+}
